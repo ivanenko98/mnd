59bbb9c0140024b0f85c837683324dd94dbac8e4
Олег Иваненко <ivanenko.oleg.m@gmail.com>
2021-05-26 22:55:19 +0300

    working on localization

--- resources/js/lang/index.js
+++ resources/js/lang/index.js
@@ -3,14 +3,10 @@ import VueI18n from 'vue-i18n';
 import Cookies from 'js-cookie';
 import elementEnLocale from 'element-ui/lib/locale/lang/en'; // element-ui lang
 import elementRuLocale from 'element-ui/lib/locale/lang/ru-RU'; // element-ui lang
-import elementZhLocale from 'element-ui/lib/locale/lang/zh-CN';// element-ui lang
-import elementViLocale from 'element-ui/lib/locale/lang/vi';// element-ui lang
 import elementUkViLocale from 'element-ui/lib/locale/lang/ua';// element-ui lang
 import ukLocale from './uk';
 import enLocale from './en';
 import ruLocale from './ru';
-import zhLocale from './zh';
-import viLocale from './vi';
 
 Vue.use(VueI18n);
 
@@ -26,14 +22,6 @@ const messages = {
   ru: {
     ...ruLocale,
     ...elementRuLocale,
-  },
-  zh: {
-    ...zhLocale,
-    ...elementZhLocale,
-  },
-  vi: {
-    ...viLocale,
-    ...elementViLocale,
   },
 };
 
